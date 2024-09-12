2434d137e0776e2e8b69bb52fca2a7cd3068378b
Ndimah Mike <ndimah22@protonmail.com>
2024-09-12 12:32:13 +0100

    feat: setup the first invoice markup with nunjucks

---
+++ eleventy.config.js
@@ -0,0 +1,36 @@
+/*jslint node*/
+const {transform} = require("lightningcss");
+const {readFile, readdir, writeFile} = require("node:fs");
+const {promisify} = require("node:util");
+const path = require("node:path");
+const fs = {};
+
+fs.readFile = promisify(readFile);
+fs.readDir = promisify(readdir);
+fs.WriteFile = promisify(writeFile);
+
+async function parseCss({dir}, src) {
+    const blob = await fs.readFile(path.normalize(
+        `${dir.input}/${dir.includes}/${src}`
+    ));
+    const {code} = transform({
+        code: blob,
+        filename: "style.css",
+        minify: true
+    });
+    return code.toString();
+}
+module.exports = function (config) {
+    config.addNunjucksAsyncShortcode("cssmin", async function (src) {
+        let result = await parseCss(config, src);
+        return result;
+    });
+
+    return {
+        dir: {
+            includes: "_templates",
+            input: "src",
+            output: "_site"
+        }
+    };
+};
---
+++ src/index.11tydata.js
@@ -0,0 +1,36 @@
+/*jslint node*/
+module.exports = Object.freeze({
+    client: {
+        address: "19 Union Terrace, London(UK)",
+        email: "john@doemail.com",
+        id: "V20392O892PK",
+        name: "John Doe",
+        tel: "+230 45 90 873"
+    },
+    due_date: "20 August 2024",
+    id: "EN3456901",
+    invoice_date: "20 May 2024",
+    items: [
+        {
+            description: "Design Services",
+            quantity: 100,
+            total: "$5,000",
+            unitPrice: "$50"
+        },
+        {
+            description: "Web Development",
+            quantity: 100,
+            total: "$25,000",
+            unitPrice: "$250"
+        }
+    ],
+    items_total: "$30,000",
+    sender: {
+        address: "64 Rue Foch, Akwa-Douala",
+        email: "kndemgne@press.com",
+        id: "P09782763780284754NU",
+        name: "Kamga Ndemgne",
+        tel: "+33 01 908 872 764"
+    },
+    tax: "$0"
+});
